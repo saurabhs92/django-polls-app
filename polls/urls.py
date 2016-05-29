bd2076022ea7fa83eb409c706ed43479b838d966
saurabhs92 <saurabhs92@gmail.com>
2016-05-29 21:14:46 +0530

    3 new urls detail, votes, results for each question.

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #Ex: /polls/
     url(r'^$', views.index, name='index'),
+    #Ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #Ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    #Ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
