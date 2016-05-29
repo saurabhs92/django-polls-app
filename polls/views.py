bb8141fff25377e1771b0520e84fc3e1314890b0
saurabhs92 <saurabhs92@gmail.com>
2016-05-30 01:35:05 +0530

    Modified question detail template to list all choices.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 
 # Create your views here.
@@ -14,7 +14,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
