d017cf70ef22316cb68458bde564625f0cf4bb31
saurabhs92 <saurabhs92@gmail.com>
2016-06-07 23:09:05 +0530

    Added code to count no. of votes of each question.

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 #from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Question, Choice
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
@@ -23,5 +24,18 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    try: 
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.", 
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
