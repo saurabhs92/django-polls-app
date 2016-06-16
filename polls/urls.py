123851fef6d7e156ad7bdc1b9b2f606b03d19953
saurabhs92 <saurabhs92@gmail.com>
2016-06-17 00:58:33 +0530

    Testcase to check was_published_recently() for future dates.

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #Ex: /polls/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #Ex: /polls/5/results/
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.s_view(), name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.DetailView.as_view(), name='results'),
     #Ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
