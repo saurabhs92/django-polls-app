1627baa4054c55f0b4b3b2c76999046f764ea112
saurabhs92 <saurabhs92@gmail.com>
2016-07-02 17:04:38 +0530

    Corrected the polls index view to show only those polls whose pub_date is not in the future.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-#from django.template import loader
+from django.utils import timezone
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Question, Choice
 from django.core.urlresolvers import reverse
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
