e68776936c0d506c7e01b3a6c78652c8383512e0
saurabhs92 <saurabhs92@gmail.com>
2016-07-21 23:48:31 +0530

    Add choices option now appears as tabbed fields in the add questions page.

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
