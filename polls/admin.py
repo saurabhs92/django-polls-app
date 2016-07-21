1fe83fc6ea8a2f5e5345ca76f95db9e5a7622e3e
saurabhs92 <saurabhs92@gmail.com>
2016-07-22 00:28:55 +0530

    New filter in the sidebar which lets you filter the question list by pub_date field.

--- polls/admin.py
+++ polls/admin.py
@@ -13,6 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
 
